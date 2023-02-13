7fc5419852daa1ccc159061377b20297d5fa0903
JuanDiego-Dev <Arenas250421@gmail.com>
2023-02-12 22:06:45 -0500

    Submodilo vocero

---
+++ resources/curriculum/js/index.js
@@ -0,0 +1,19 @@
+
+// const miTexto = document.getElementById("textoEjemplo");
+// miTexto.style.color = "red";
+
+// function cambiarBlue(){
+//     miTexto.style.color = "blue"
+// }
+
+// function cambiarRed(){
+//     miTexto.style.color = "red"
+// }
+
+window.onclick = myFunction;
+
+function myFunction() {
+    document.getElementsByTagName("BODY")[0].style.backgroundColor = "rgb(77, 71, 71)";
+}
+
+// background: rgb(207, 196, 196);
