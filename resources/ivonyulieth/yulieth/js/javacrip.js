ea4e0ec8f0b6d770298807c411239f3158b7ce8f
ivonej <yuliedyvon@gmail.com>
2023-05-09 06:51:33 -0500

    vf

---
+++ resources/ivonyulieth/yulieth/js/javacrip.js
@@ -0,0 +1,18 @@
+const boton = document.querySelector(".btncolor");
+const fondo = document.querySelector("body");
+const colores = [
+    "pink",
+    "blue",
+    "yellow",
+    "green",
+    "orange",
+]
+
+function coloraleAtorio(){
+    return colores[Math.floor(Math.random()*colores.length)];
+}
+boton.addEventListener("click", function () {
+    const color = coloraleAtorio();
+    fondo.style.background = color;
+    console.log('si ha cambiado');
+});
