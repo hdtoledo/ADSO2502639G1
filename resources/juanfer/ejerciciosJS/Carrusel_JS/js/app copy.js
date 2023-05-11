7c50afc1c0a4d6224f1fbe1b44511690218a9bf1
Ivon <yuliedyvon@gmail.com>
2023-05-11 11:29:42 -0500

    vp

--- resources/juanfer/ejerciciosJS/Carrusel_JS/js/app copy.js
+++ resources/juanfer/ejerciciosJS/Carrusel_JS/js/app copy.js
@@ -1,9 +1,9 @@
-
+//* Variables
 let containerImg = document.getElementById('containerImg')
-const arrayImgs = ["url('./img/img-01.jpg')","url('./img/img-02jpg')","url('./img/img-03jpg')","url('./img/img.-04jpg')"]
+const arrayImgs = ["url('./img/img-01.jpg')","url('./img/img-02.jpg')","url('./img/img-03.jpg')","url('./img/img-04.jpg')","url('./img/img-05.jpg')","url('./img/img-06.jpg')","url('./img/img-07.jpg')"]
 let iterator = 0
 
-
+//* Btns
 let btn_right = document.getElementById('btn-right')
 let btn_left = document.getElementById('btn-left')
 
@@ -27,6 +27,4 @@ btn_left.addEventListener('click', () => {
     }
     console.log(iterator)
 })
-
-
 
