064d618cfb54b7529f4b704334bbed26b59c6a57
Tatianaangarita26 <tatianaangarita150@gmail.com>
2023-05-29 07:05:52 -0500

    juego

--- resources/karoltatiana/pages/juego/js/java.js
+++ resources/karoltatiana/pages/juego/js/java.js
@@ -11,34 +11,3 @@ const puntajeMaquina = document.querySelector('.r-maquina p');
 
 const opciones = ['piedra', 'papel', 'tijera'];
 
-// generar la elección aleatoria de la máquina
-function generarEleccionMaquina() {
-  const indice = Math.floor(Math.random() * opciones.length);
-  return opciones[indice];
-}
-
-//determinar el resultado del juego
-function determinarResultado(eleccionUsuario, eleccionMaquina) {
-  if (eleccionUsuario === eleccionMaquina) {
-    return 'Empatado';
-  } else if (
-    (eleccionUsuario === 'piedra' && eleccionMaquina === 'tijera') ||
-    (eleccionUsuario === 'papel' && eleccionMaquina === 'piedra') ||
-    (eleccionUsuario === 'tijera' && eleccionMaquina === 'papel')
-  ) {
-    return 'Ganado';
-  } else {
-    return 'Perdido';
-  }
-}
-
-// Función para manejar la elección del usuario
-function manejarEleccionUsuario(eleccion) {
-  const eleccionMaquina = generarEleccionMaquina();
-  const resultado = determinarResultado(eleccion, eleccionMaquina);
-
-  usuarioEleccion.textContent = `El usuario eligió: ${eleccion}`;
-  maquinaEleccion.textContent = `La máquina eligió: ${eleccionMaquina}`;
-  resultadoText.textContent = resultado;
-
-}
