3b6c8f641e9a7c4e65aa065e35bea3294903fecf
julian <protolito16@gmail.com>
2023-06-15 07:31:08 -0500

    vf2

--- resources/luisgamba/trabajos/api-movies/js/script.js
+++ resources/luisgamba/trabajos/api-movies/js/script.js
@@ -18,7 +18,7 @@ btnAnterior.addEventListener('click', () => {
 
 const cargarPeliculas = async() => {
     try {
-        const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=`)
+        const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`);
 
         console.log(respuesta);
 
