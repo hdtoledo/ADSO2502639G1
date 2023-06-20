4330feae689dc1d3db07c8c7beec108a50d796c1
Tatianaangarita26 <tatianaangarita150@gmail.com>
2023-06-20 14:35:19 -0500

    portafolio

--- resources/karoltatiana/pages/ApiMovies/js/java.js
+++ resources/karoltatiana/pages/ApiMovies/js/java.js
@@ -3,7 +3,7 @@ const btnAnterior = document.getElementById('btnAnterior');
 const btnSiguiente = document.getElementById('btnSiguiente');
 
 btnSiguiente.addEventListener('click', () => {
-    if(pagina < 1000){
+    if(pagina <1000){
         pagina += 1;
         cargarPeliculas();
     }
@@ -16,42 +16,39 @@ btnAnterior.addEventListener('click', () => {
     }
 });
 
-
 const cargarPeliculas = async() => {
     try {
         const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`);
 
         console.log(respuesta);
 
-        //Si la respuesta es correcta
-
-        if(respuesta.status === 200){
+        // Si la respuesta es correcta 
+        if(respuesta.status == 200){
             const datos = await respuesta.json();
 
             let peliculas = '';
             datos.results.forEach(pelicula => {
                 peliculas += `
-                <div class="peliculas">
-                <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
-                <h3 class="titulo">${pelicula.title}</h3>
-                </div>
+                    <div class="pelicula">
+                        <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+                        <h3 class="titulo">${pelicula.title}</h3>
+                    </div>
                 `;
+
             });
 
             document.getElementById('contenedor').innerHTML = peliculas;
 
-        }else if(respuesta.status === 401){
+        } else if(respuesta.status == 401){
             console.log('Pusiste la llave mal');
-        }else if(respuesta.status === 404){
+        } else if(respuesta.status == 404){
             console.log('La pelicula que buscas no existe');
-        }else {
+        } else {
             console.log('Hubo un error y no sabemos que paso');
         }
-
-        } catch(error){
-            console.log(error);
-        }
-
+    } catch(error){
+        console.log(error);
     }
+}
 
-    cargarPeliculas();
+cargarPeliculas();
