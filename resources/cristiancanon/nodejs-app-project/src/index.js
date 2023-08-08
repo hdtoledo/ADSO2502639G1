42259878d22c2dec092aa029e18e9495f8093ee3
EstebanR054 <esteban.roa7@soy.sena.edu.co>
2023-08-08 11:04:13 -0500

    version

---
+++ resources/cristiancanon/nodejs-app-project/src/index.js
@@ -0,0 +1,18 @@
+//user/
+
+import express from "express"
+import {dirname} from 'path'
+import { fileURLToPath } from "url"
+
+const app = express()
+
+dirname(fileURLToPath(import.meta.url))
+
+app.set('views')
+
+const _dirname = dirmane(fileURLToPath(import.meta.url))
+console.log(_dirmane)
+
+
+
+
